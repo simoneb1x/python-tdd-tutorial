f4c3431f9311c1bd1c534e214c1c07c09c78773c
simoneb1x <moneb1x@gmail.com>
2022-03-03 00:17:17 +0100

    green phase passed

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,4 +1,16 @@
 # Main script for FizzBuzz
 
-def FizzBuzz():
-    pass
+def FizzBuzz(value):
+    output = []
+
+    for number in range(1, value+1):
+        if number % 15 == 0:
+            output.append("FizzBuzz")
+        elif number % 3 == 0:
+            output.append("Fizz")
+        elif number % 5 == 0:
+            output.append("Buzz")
+        else:
+            output.append(number)
+    
+    return output
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -12,7 +12,7 @@ class FizzBuzzTests(unittest.TestCase):
         self.assertEqual(FizzBuzz(5), [1, 2, "Fizz", 4, "Buzz"])
 
     def test_if_value_is_a_multiple_of_three_and_five_use_fizzbuzz(self):
-        self.assertEqual(FizzBuzz(5), [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
+        self.assertEqual(FizzBuzz(15), [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
 
 if __name__ == '__main__':
     unittest.main()
