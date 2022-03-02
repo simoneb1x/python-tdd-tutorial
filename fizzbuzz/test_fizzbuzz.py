7137ff5c0dd10ed2ac6ca9f27839dc556eb2a35a
simoneb1x <moneb1x@gmail.com>
2022-03-03 00:27:23 +0100

    added a multiline comment on test_fizzbuzz for acceptance criteria

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,12 @@
-# Testing FizzBuzz
+"""
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+"""
 
 import unittest
 from fizzbuzz import FizzBuzz
