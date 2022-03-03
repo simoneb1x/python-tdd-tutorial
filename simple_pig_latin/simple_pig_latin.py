92075fd82c075aad8c9e4f9fc0740d605089fde1
simoneb1x <moneb1x@gmail.com>
2022-03-03 10:36:15 +0100

    all test succeed -> green phase, let's clean up

--- simple_pig_latin/simple_pig_latin.py
+++ simple_pig_latin/simple_pig_latin.py
@@ -1,16 +1,25 @@
+import string
+
 def pig_it(text):
-    # first array where I split the words based on whitespaces
+    # first list where I split the words based on whitespaces
     text_splitted = text.split()
 
     # array that will be returned as output
     output = []
     
+    # looping the list:
+    # if word == punctuation, just simply append it
+    # else, transform the word and append it
     for word in text_splitted:
-        word_splitted = list(word)
-        word_splitted.append(word_splitted[0] + "ay")
-        word_splitted.pop(0)
-        output.append(''.join(word_splitted))
+        if word in string.punctuation:
+            output.append(word)
+        else:
+            word_splitted = list(word)
+            word_splitted.append(word_splitted[0] + "ay")
+            word_splitted.pop(0)
+            output.append(''.join(word_splitted))
 
+    # we join all words in a single string
     output = ' '.join(output)
 
     return output
--- simple_pig_latin/test_simple_pig_latin.py
+++ simple_pig_latin/test_simple_pig_latin.py
@@ -12,6 +12,12 @@ from simple_pig_latin import pig_it
 class PigLatinTest(unittest.TestCase):
 
     def test_pig_latin_is_cool(self):
+        self.assertEqual(pig_it("Alea iacta est"), "leaAay actaiay steay")
+
+    def test_lux_in_tenebris_lucet(self):
+        self.assertEqual(pig_it("Lux in tenebris lucet"), "uxLay niay enebristay ucetlay")
+
+    def test_pig_latin_is_cool(self):
         self.assertEqual(pig_it("Pig latin is cool"), "igPay atinlay siay oolcay")
 
     def test_hello_world_with_punctuation(self):
@@ -25,3 +31,9 @@ class PigLatinTest(unittest.TestCase):
 
     def test_cancel_the_daily_scrum(self):
         self.assertEqual(pig_it("Today is a good day"), "odayTay siay aay oodgay ayday")
+
+    def test_quis_custodiet_ipsos_custodes(self):
+        self.assertEqual(pig_it("Quis custodiet ipsos custodes ?"), "uisQay ustodietcay psosiay ustodescay ?")
+    
+    def test_o_tempora_o_mores(self):
+        self.assertEqual(pig_it("O tempora o mores !"), "Oay emporatay oay oresmay !")
