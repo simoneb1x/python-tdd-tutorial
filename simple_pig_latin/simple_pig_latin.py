f918eb767f9c5166983bb5600e04af174425f6f1
simoneb1x <moneb1x@gmail.com>
2022-03-03 10:26:37 +0100

    function defined, only one test is failing

--- simple_pig_latin/simple_pig_latin.py
+++ simple_pig_latin/simple_pig_latin.py
@@ -1,2 +1,16 @@
 def pig_it(text):
-    pass
+    # first array where I split the words based on whitespaces
+    text_splitted = text.split()
+
+    # array that will be returned as output
+    output = []
+    
+    for word in text_splitted:
+        word_splitted = list(word)
+        word_splitted.append(word_splitted[0] + "ay")
+        word_splitted.pop(0)
+        output.append(''.join(word_splitted))
+
+    output = ' '.join(output)
+
+    return output
