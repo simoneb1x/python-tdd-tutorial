30adadb4caeb2c723eda788c57b9e6cd5feac3f4
simoneb1x <moneb1x@gmail.com>
2022-03-03 12:13:26 +0100

    added human readable time exercise, red phase

---
+++ human_readable_time/test_human_readable_time.py
@@ -0,0 +1,24 @@
+"""
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+"""
+
+import unittest
+from human_readable_time import make_readable
+
+class HumanReadableTimeTest(unittest.TestCase):
+    def test_zero_seconds(self):
+        self.assertEqual(make_readable(0), "00:00:00")
+
+    def test_ten_seconds(self):
+        self.assertEqual(make_readable(5), "00:00:05")
+
+    def test_ten_minutes(self):
+        self.assertEqual(make_readable(600), "00:10:00")
+    
+    def test_one_hour(self):
+        self.assertEqual(make_readable(3600), "01:00:00")
