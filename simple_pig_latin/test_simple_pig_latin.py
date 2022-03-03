ae43248e0e33fc601fa12d3e50dfc5c53a888853
simoneb1x <moneb1x@gmail.com>
2022-03-03 10:11:36 +0100

    simple pig latin started, 3 tests wrote. they failed -> red phase

---
+++ simple_pig_latin/test_simple_pig_latin.py
@@ -0,0 +1,21 @@
+"""
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+Examples:
+
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+"""
+
+import unittest
+from simple_pig_latin import pig_it
+
+class PigLatinTest(unittest.TestCase):
+
+    def test_pig_latin_is_cool(self):
+        self.assertEqual(pig_it("Pig latin is cool"), "igPay atinlay siay oolcay")
+
+    def test_hello_world_with_punctuation(self):
+        self.assertEqual(pig_it("Hello world !"), "elloHay orldway !")
+
+    def test_this_is_my_string(self):
+        self.assertEqual(pig_it("This is my string"), "hisTay siay ymay tringsay")
