68484987f3b91b5cbf103030e8a027f91488239f
simoneb1x <moneb1x@gmail.com>
2022-03-07 23:09:39 +0100

    green phase, human readable duration format

--- human_readable_duration_format/human_readable_duration_format.py
+++ human_readable_duration_format/human_readable_duration_format.py
@@ -1,2 +1,28 @@
+def form(time, seconds_converted):
+    if time == 0:
+        return ""
+    if time == 1:
+        return "1 "+ seconds_converted
+    return str(time) + " " + seconds_converted + "s"
+
+def readable(array):
+    array = list(filter(None, array))
+    result = ""
+    if len(array) == 1:
+        return array[0]
+    for i in range(len(array)-1):
+        result = result + ", " + array[i]
+    result = result + " and " + array[len(array)-1]
+    return result[2:]
+
 def format_duration(seconds):
-    pass
+    if seconds == 0:
+        return "now"
+
+    y = seconds // (3600*24*365)
+    d = (seconds % (3600*24*365))//(3600*24)
+    h = (seconds % (3600*24)) // 3600
+    m = (seconds % 3600) // 60
+    s = seconds % 60
+
+    return readable([form(y,"year"),form(d,"day"),form(h,"hour"),form(m,"minute"),form(s,"second")])
