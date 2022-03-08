8a507f4119dceefdc9b0c9577b92fe167fd532d2
simoneb1x <moneb1x@gmail.com>
2022-03-08 22:59:16 +0100

    added middle permutation exercise. red phase

---
+++ middle_permutation/test_middle_permutation.py
@@ -0,0 +1,26 @@
+"""
+You are given a string s. Every letter in s appears once.
+
+Consider all strings formed by rearranging the letters in s. 
+After ordering these strings in dictionary order, return the middle term. (If the sequence has a even 
+length n, define its middle term to be the (n/2)th term.)
+"""
+
+import unittest
+from middle_permutation import middle_permutation
+
+class HumanReadableTimeTest(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(middle_permutation("abc"),"bac")
+
+    def test_two(self):
+        self.assertEqual(middle_permutation("abcd"),"bdca")
+
+    def test_three(self):
+        self.assertEqual(middle_permutation("abcdx"),"cbxda")
+    
+    def test_four(self):
+        self.assertEqual(middle_permutation("abcdxg"),"cxgdba")
+
+    def test_five(self):
+        self.assertEqual(middle_permutation("abcdxgz"),"dczxgba")
